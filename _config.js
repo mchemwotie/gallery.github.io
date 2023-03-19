1a70ab805fd446cabe50a347fddedbb0cfd3af18
mchemwotie <115400555+mchemwotie@users.noreply.github.com>
2023-03-19 11:03:55 +0300

    Add files via upload

---
+++ _config.js
@@ -0,0 +1,9 @@
+var config = {}
+
+// Update to have your correct username and password
+config.mongoURI = {
+    production: 'mongodb+srv://<mercychemwotie>:<Idiots123#>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
+    development: 'mongodb+srv://<mercychemwotie>:<Idiots123#>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
+    test: 'mongodb+srv://<mercychemwotie>:<Idiots123#>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+}
+module.exports = config;
